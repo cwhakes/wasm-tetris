e271f9cb1825810e2915662ab7da1ba765b42551
Will Hakes <charles.hakes@colorado.edu>
2017-12-25 17:36:10 -0700

    Include broken out position

---
+++ src/geometry/position.rs
@@ -0,0 +1,19 @@
+#[derive(Debug)]
+pub struct Position {
+    pub x: i16,
+    pub y: i16,
+}
+
+impl Position {
+    pub fn rotate_sunwise(&mut self) {
+        let temp = self.y;
+        self.y = -self.x;
+        self.x = temp;
+    }
+    
+    pub fn rotate_widdershins(&mut self) {
+        let temp = self.x;
+        self.x = -self.y;
+        self.y = temp;
+    }
+}
